ae276ed24df1f0717e34d4ebdf0bc30bd0a0f32c
Emma Kinch <eokinch@gmail.com>
2017-08-29 17:01:17 -0400

    upadting js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,30 @@
 const ohHey = "Hello World";
 
 console.log(ohHey);
+
+var app = {};
+
+rooster.init = function(){
+	$.ajax({
+		url: 'http://proxy.hackeryou.com',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		data: JSON.stringify({
+			reqUrl: 'https://accounts.spotify.com/api/token',
+			params: {
+				grant_type: 'client_credentials'
+			},
+			proxyHeaders: {
+				'Authorization': 'Basic NWFkYzVlNjQ4ZmVlNGVmMjg2NjYwMjYyOTc1NGNhMWE6NDNmZjFjNjUzYzQyNGY5Mzk2NjI1OTJkN2EyMTJmYTk'
+			}
+		})
+	})
+	.then((data) => {
+		headers = {
+			'Authorization': `${data.token_type} ${data.access_token}`
+		}
+	})
+}
