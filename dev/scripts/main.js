c0f133747388de0e15e97f02048bd86a2b0d4da5
Colm Whitford <colmwhitford@gmail.com>
2017-09-01 17:01:58 -0400

    added footer and error message styles

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -42,7 +42,6 @@ musicApp.getCategories = function() {
 			limit: 50
 		}
 	}).then(function(res){
-		console.log(res.categories['items'])
 		musicApp.updateSelect(res.categories['items'])
 	})
 };
@@ -54,10 +53,6 @@ musicApp.updateSelect = function(data) {
 		}
 	})
 }
-// go through every item and if it does not include the ID from not genre
-// forEach ID in array check against musicApp.notGenres array 
-// if strings match don't do anything
-// if strings don't match grab name of genre (for the select option) and id of genre (for the value of the select), store in variables and template literal them
 
 musicApp.getPlaylists = function(genre) {
 	$.ajax({
@@ -69,19 +64,15 @@ musicApp.getPlaylists = function(genre) {
 			limit: 50
 		}
 	}).then(function(res) {
-		console.log(res)
 		musicApp.getRandomPlaylist(res.playlists.items)
 	})
 };
 
 musicApp.getRandomPlaylist = function(playlists) {
-	console.log(playlists.length)
 	let randomIndex = Math.floor(Math.random() * playlists.length);
 	let chosenPlaylist = playlists[randomIndex].tracks.href;
 	musicApp.URI = playlists[randomIndex].uri;
-	// console.log(musicApp.URI);
 	musicApp.getTracks(chosenPlaylist);
-	console.log(chosenPlaylist)
 };
 
 musicApp.getTracks = function(playlist) {
@@ -95,29 +86,24 @@ musicApp.getTracks = function(playlist) {
 	})
 };
 musicApp.checkTracks = (res) => {
-	console.log(res)
 	let tracks = res.items;
 	tracks.forEach(function (track){
 		if(track.track.explicit === true){
 			musicApp.spiceCount += 1;
 		}		
 	})
-	console.log(musicApp.desiredSpice)
 	let spicyPercentage = Math.floor((musicApp.spiceCount / tracks.length) * 100);
 	if(spicyPercentage <= musicApp.desiredSpice){
-		// console.log('working')
 		musicApp.displayPlaylist();
 	} else {
 		musicApp.spiceCount = 0;
 		musicApp.attempt++;
-		console.log('attempt', musicApp.attempt);
 		if (musicApp.attempt < 50) {
 			musicApp.getPlaylists(musicApp.genre);
 		}else{
 			musicApp.displayError();
 		}
 	}
-	console.log(spicyPercentage);
 };
 musicApp.displayPlaylist = () => {
 	$('.output').append(`<iframe src="https://open.spotify.com/embed?uri=${musicApp.URI}&view=list"
@@ -125,16 +111,9 @@ musicApp.displayPlaylist = () => {
 }
 musicApp.displayError = () => {
 	let genre = $('.genre:selected').text();
-	$('.output').html(`<p>There is no ${genre} playlist at a ${musicApp.desiredSpice} Spice level</p>`);
+	$('.output').html(`<p class="playlistError">Sorry, there are no ${genre} playlists at a ${musicApp.desiredSpice} spice level.</p>`);
 }
 
-// if explicit is = to true add 1 to the spice count
-// then compare that number to the # of songs in the playlist 
-// caculate percantage of playlist that is spicy
-// then compare that to the users allowed spicyness
-// if equlivent or less display playslist
-// if greater than user choice generate another playlist and repeat
-// if nothing is bland enough display error message for user
 musicApp.colorChilis = function(chiliValue) {
 	$('svg').removeClass('redChili');
 	if(chiliValue === '0'){
@@ -152,16 +131,13 @@ musicApp.colorChilis = function(chiliValue) {
 		$('#chili0, #chili1, #chili2, #chili3, #chili4, #chili5').addClass('redChili');
 	};
 };
-// if chili value is equal to 0, color first chili
-// if 1, color chili1 and chili2
-// if 2, color chili1 chili2 chili3
+
 musicApp.events = function(){
 	$('form').on('submit',function(event){
 		$('.output').empty();
 		event.preventDefault();
 		musicApp.genre = $('#genres').val();	 
 		musicApp.desiredSpice = $('#spiceLevel').val();
-		console.log(musicApp.desiredSpice);
 		musicApp.getPlaylists(musicApp.genre);
 		musicApp.spiceCount = 0;
 		musicApp.attempt = 0;
